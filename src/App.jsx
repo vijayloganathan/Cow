7af0497554cc8736890df7f0bed89f9209173635
vijayloganathan <vijayloganathan2002@gmail.com>
2024-09-23 12:31:03 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
         <Routes>
           {/* <Route path="/" element={<Landingpage />} />
           <Route path="/home/*" element={<Home />} /> */}
-          App
+          <div>App vijay</div>
         </Routes>
       </Router>
     </>
